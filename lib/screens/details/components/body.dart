a649497a7b3f08d4216763c2226db5fe248c4c5d
chukschiboy <chukschiboy@gmail.com>
2020-07-02 22:01:55 +0100

    update on details

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -6,6 +6,8 @@ import 'package:movie_box/models/movie.dart';
 import 'package:movie_box/screens/details/components/title_duration_and_fav_btn.dart';
 
 import 'backdrop_rating.dart';
+import 'cast_and_crew.dart';
+import 'genres.dart';
 
 class Body extends StatelessWidget {
   final Movie movie;
@@ -15,25 +17,36 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     // it will provide us total height and width
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        BackdropAndRating(size: size, movie: movie),
-        SizedBox(height: kDefaultPadding / 2),
-        TitleDurationAndFavBtn(movie: movie),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-          child: SizedBox(
-            height: 36,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: movie.genra.length,
-              itemBuilder: (context, index) => GenreCard(
-                genre: movie.genra[index],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          BackdropAndRating(size: size, movie: movie),
+          SizedBox(height: kDefaultPadding / 2),
+          TitleDurationAndFavBtn(movie: movie),
+          Genres(movie: movie),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: kDefaultPadding / 2, 
+              horizontal: kDefaultPadding,
+            ),
+            child: Text(
+              "Plot Summary", 
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding ),
+            child: Text(
+              movie.plot, 
+              style: TextStyle(
+                color: Color(0xFF737599),
               ),
             ),
           ),
-        ),
-      ],
+          CastAndCrew(casts: movie.cast),
+        ],
+      ),
     );
   }
 }
